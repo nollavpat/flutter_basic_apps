d3939c6efb9ea85c0178c63a6fad99b4eb16f1c8
Patrick Vallon <jpatrickvallon@gmail.com>
2022-02-06 18:32:42 +0800

    Use ListView.builder

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,46 +10,51 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var phCurrency = NumberFormat.currency(locale: 'en_US', symbol: '₱');
+
     return SizedBox(
       height: 300,
-      child: ListView(
-          children: transactions
-              .map((tx) => Card(
-                    child: Row(
-                      children: [
-                        Container(
-                          margin: const EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 15),
-                          decoration: BoxDecoration(
-                              border:
-                                  Border.all(color: Colors.purple, width: 2)),
-                          padding: const EdgeInsets.all(10),
-                          child: Text(
-                            '₱${tx.amount.toStringAsFixed(2)}',
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.purple),
-                          ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              tx.title,
-                              style: const TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              DateFormat('yMMMEd').format(tx.date),
-                              style: const TextStyle(color: Colors.grey),
-                            ),
-                          ],
-                        )
-                      ],
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          var tx = transactions[index];
+
+          return Card(
+            child: Row(
+              children: [
+                Container(
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.purple, width: 2)),
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    phCurrency.format(tx.amount),
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      tx.title,
+                      style: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold),
                     ),
-                  ))
-              .toList()),
+                    Text(
+                      DateFormat('yMMMEd').format(tx.date),
+                      style: const TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
+      ),
     );
   }
 }
