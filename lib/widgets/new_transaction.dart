514afbe604e7bb2ccac1e4aeddf11140b966fd69
Patrick Vallon <jpatrickvallon@gmail.com>
2022-02-06 19:25:40 +0800

    Basic text field vaidation

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,4 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+// import 'package:intl/intl.dart';
+
+// class CurrencyInputFormatter extends TextInputFormatter {
+//   @override
+//   TextEditingValue formatEditUpdate(
+//       TextEditingValue oldValue, TextEditingValue newValue) {
+//     if (newValue.selection.baseOffset == 0) {
+//       return newValue;
+//     }
+
+//     double value = double.parse(newValue.text);
+
+//     final formatter = NumberFormat.currency(locale: 'en_US', symbol: '₱');
+
+//     String newText = formatter.format(value);
+
+//     return newValue.copyWith(
+//         text: newText,
+//         selection: TextSelection.collapsed(offset: newText.length));
+//   }
+// }
 
 class NewTransaction extends StatelessWidget {
   final titleController = TextEditingController();
@@ -9,6 +31,20 @@ class NewTransaction extends StatelessWidget {
     Key? key,
     required this.addTx,
   }) : super(key: key);
+
+  void submitData() {
+    final enteredText = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredText.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addTx(
+      enteredText,
+      enteredAmount,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,12 +62,25 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: const InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType:
+                  const TextInputType.numberWithOptions(decimal: true),
+              inputFormatters: [
+                FilteringTextInputFormatter.allow(RegExp(r"[0-9.]")),
+                TextInputFormatter.withFunction((oldValue, newValue) {
+                  try {
+                    final text = newValue.text;
+                    if (text.isNotEmpty) double.parse(text);
+                    return newValue;
+                    // ignore: empty_catches
+                  } catch (e) {}
+                  return oldValue;
+                }),
+              ],
+              onSubmitted: (_) =>
+                  submitData(), // used in mobile keyboard check key (lower left)
             ),
             TextButton(
-                onPressed: () {
-                  addTx(titleController.text,
-                      double.parse(amountController.text));
-                },
+                onPressed: submitData,
                 child: const Text(
                   'Add Transaction',
                   style: TextStyle(color: Colors.purple),
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,7 +10,7 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var phCurrency = NumberFormat.currency(locale: 'en_US', symbol: '₱');
+    final phCurrency = NumberFormat.currency(locale: 'en_US', symbol: '₱');
 
     return SizedBox(
       height: 300,
